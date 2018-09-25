1c4acd8e09bb37347a033fd076ec81944f0a80ea
Anton Classon <anton.classon@gmail.com>
2018-09-25 15:32:59 +0200

    Created product filter

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -91,13 +91,12 @@ namespace MyShop.WebUI.Controllers
 
             if(file != null)
             {
-                product.Image = product.Id + Path.GetExtension(file.FileName);
-                file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
             }
 
             productToEdit.Category = product.Category;
             productToEdit.Description = product.Description;
-            productToEdit.Image = product.Image;
             productToEdit.Name = product.Name;
             productToEdit.Price = product.Price;
 
